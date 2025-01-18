024078010994666d295ee7cae49da63f24c48fbe
wilayat8070 <wilayat@360i.agency>
2025-01-18 20:59:11 +0500

    wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,55 @@
-import Image from "next/image";
+// import { Icons } from '@/components/Icons'
+import MaxWidthWrapper from './components/MaxWidthWrapper'
+// import Phone from '@/components/Phone'
+// import { Reviews } from '@/components/Reviews'
+// import { buttonVariants } from '@/components/ui/button'
+import { ArrowRight, Check, Star } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      my mob-case-dev wilayat ali ecommerce project
-    </main>
-  );
+    <div className='bg-slate-50 grainy-light'>
+      <section>
+        <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+          <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
+            <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
+              <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
+                <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
+                {/* <img src='/snake-1.png' className='w-full' /> */}
+              </div>
+              <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
+                Your Image on a{' '}
+                <span className='bg-green-600 px-2 text-white'>Custom</span>{' '}
+                Phone Case
+              </h1>
+              <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
+                Capture your favorite memories with your own,{' '}
+                <span className='font-semibold'>one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+
+              <ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+                <div className='space-y-2'>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />
+                    High-quality, durable material
+                  </li>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />5 year
+                    print guarantee
+                  </li>
+                  <li className='flex gap-1.5 items-center text-left'>
+                    <Check className='h-5 w-5 shrink-0 text-green-600' />
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  )
 }
