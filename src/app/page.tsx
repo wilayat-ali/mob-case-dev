794e4b3cdc28b08828da1d376ec233d98e6754a3
wilayat8070 <wilayat@360i.agency>
2025-01-19 20:03:09 +0500

    kinde auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import { Icons } from '@/components/Icons'
 import MaxWidthWrapper from './components/MaxWidthWrapper'
-// import Phone from '@/components/Phone'
+import Phone from './components/Phone'
 // import { Reviews } from '@/components/Reviews'
 // import { buttonVariants } from '@/components/ui/button'
 import { ArrowRight, Check, Star } from 'lucide-react'
@@ -16,7 +16,7 @@ export default function Home() {
               <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
                 {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
                 <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
-                {/* <img src='/snake-1.png' className='w-full' /> */}
+                <img src='/snake-1.png' className='w-full' />
               </div>
               <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
                 Your Image on a{' '}
@@ -46,6 +46,64 @@ export default function Home() {
                   </li>
                 </div>
               </ul>
+
+              <div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
+                <div className='flex -space-x-4'>
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-1.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-2.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-3.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-4.jpg'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-5.jpg'
+                    alt='user image'
+                  />
+                </div>
+
+                <div className='flex flex-col justify-between items-center sm:items-start'>
+                  <div className='flex gap-0.5'>
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                    <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  </div>
+
+                  <p>
+                    <span className='font-semibold'>1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className='relative md:max-w-xl'>
+              <img
+                src='/your-image.png'
+                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+              />
+              <img
+                src='/line.png'
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
             </div>
           </div>
         </MaxWidthWrapper>
